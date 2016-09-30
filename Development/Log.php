0904426f7eaccf2877388efde03c1fa8e91c9ce6
LegionLab <leonardo-i@outlook.com>
2016-09-30 15:16:33 -0300

    change var dir

--- Development/Log.php
+++ Development/Log.php
@@ -20,7 +20,7 @@ class Log
      * Atributos privados
      * @var string - diretório onde se armazenará arquivos de log
      */
-    private static $dir = 'logs/';
+    private static $dir = ROOT.'logs/';
 
     /**
      * Registra um novo log, o log padrao é mysql_errors, onde são salvos os erros do banco de dados
@@ -34,11 +34,11 @@ class Log
     {
         if(Settings::get('logs')) {
             $date = date('Y-m-d H:i:s');
-            if(file_exists(ROOT.self::$dir . $archive)) {
+            if(file_exists(self::$dir . $archive)) {
                 $msg = "________________________________________________________\n" .
                     "___" . $date . " by " . $_SERVER['REMOTE_ADDR'] . "\n" .
                     $msg . "\n";
-                file_put_contents(ROOT.self::$dir . $archive, $msg, FILE_APPEND);
+                file_put_contents(self::$dir . $archive, $msg, FILE_APPEND);
             }
             else {
                 Errors::display('Arquivo de log não existe');
